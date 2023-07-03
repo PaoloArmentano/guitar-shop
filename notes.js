a087c39693fb1a9f7a565e6e1f5965b14fd8509b
Paolo Armentano <armentano.paolo@gmail.com>
2023-07-03 10:02:39 +0100

    started project

---
+++ notes.js
@@ -0,0 +1,21 @@
+// Have a title
+// nav bar
+// grid of guitars
+// the guitar card should have: name, in stock or not, price, maybe rating, add to basket
+// use constructor function to have new guitars added
+// if you wanna have rating either hard code it or even better use a loop
+// when you click on a guitar, the page opens with the description of the guitar, the price again, add to basket again and a video to show what it sounds like 
+// put opening hours somewhere
+// f
+// f
+// f
+// f
+// f
+// f
+// f
+// f
+// f
+// f
+// f
+// f
+// f
---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+console.log("Welcome to my guitar shop");
+
+
+const card = document.getElementById("guitar-card");
+const img = document.getElementById("guitar-image");
+const price = document.getElementById("price");
+const stock = document.getElementById("stock");
+const button = document.getElementById("basket-btn")
+
+
+
