36d1cd225707c79a36117022b5ecc9544c0e573f
Paolo Armentano <armentano.paolo@gmail.com>
2023-07-03 13:32:48 +0100

    created card in css

--- script.js
+++ script.js
@@ -5,9 +5,21 @@ console.log("Welcome to my guitar shop");
 
 const card = document.getElementById("guitar-card");
 const img = document.getElementById("guitar-image");
-const price = document.getElementById("price");
-const stock = document.getElementById("stock");
-const button = document.getElementById("basket-btn")
+const guitarName = document.getElementById("guitar-name");
+const guitarPrice = document.getElementById("price");
+const guitarStock = document.getElementById("stock");
+const button = document.getElementById("basket-btn");
+
+const allGuitars = [];
 
 
+function Guitar(name, img, price, stock){
+    this.name = name;
+    this.img = img;
+    this.price = price;
+    this.stock = stock;
+    allGuitars.push(this);
+}
 
+const pinky = new Guitar("Fender Strat", "", "Price: £2000", "hi");
+console.log(pinky);
